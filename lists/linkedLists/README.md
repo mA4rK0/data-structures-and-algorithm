b3fa8eea6ed281cdde3691e6461785e01b91a77c
mA4rK0 <onlymarko25@gmail.com>
2024-12-10 23:01:47 +0700

    making a linked lists formula.

---
+++ lists/linkedLists/README.md
@@ -0,0 +1,5 @@
+# Linked Lists
+
+A linked list is a collection of objects called nodes. Each node is linked to a successor node in the list using an object reference. Marking the beginning of a linked list can be a problem. Many linked-list implementations include a special node, called the head, to denote the beginning of a linked list. Linked list stores Nodes in 2 parts (data + address(next Node)), elements are linked using pointers. Linked list does not have indexes like array. Linked lists are good for inserting and deleting a data in the list (O(1) constant time). But not suitable for searching and looping a lot of data (Linear time O(n)).
+
+**Resource**: <https://www.youtube.com/watch?v=N6dOwBde7-M&list=PLZPZq0r_RZON1eaqfafTnEexRzuHbfZX8&index=5>
---
+++ lists/linkedLists/formula.js
@@ -0,0 +1,46 @@
+class Node {
+  constructor(element) {
+    this.element = element;
+    this.next = null;
+  }
+}
+
+class LList {
+  constructor() {
+    this.head = new Node("head");
+  }
+
+  find(item) {
+    let currNode = this.head;
+    while (currNode.element !== item) {
+      currNode = currNode.next;
+    }
+    return currNode;
+  }
+
+  insert(newElement, item) {
+    let newNode = new Node(newElement);
+    let current = this.find(item);
+    newNode.next = current.next;
+    current.next = newNode;
+  }
+
+  display() {
+    let currNode = this.head;
+    while (!(currNode.next === null)) {
+      console.log(currNode.next.element);
+      currNode = currNode.next;
+    }
+  }
+}
+
+//exm:
+
+let cities = new LList();
+cities.insert("Conway", "head");
+cities.insert("Russellville", "Conway");
+cities.insert("Beni", "Russellville");
+cities.insert("Alma", "Beni");
+cities.display();
+
+// head -> Conway -> Russellville -> Beni -> Alma -> null
