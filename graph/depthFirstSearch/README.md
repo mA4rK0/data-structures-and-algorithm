6b6bbf7cd5b61286f2d7ba5bc63078a8059c3501
mA4rK0 <onlymarko25@gmail.com>
2024-12-29 16:51:23 +0700

    making a depth first search algorithm formula.

---
+++ graph/depthFirstSearch/README.md
@@ -0,0 +1,3 @@
+# Depth First Search
+
+Depth First Search is a search algorithm for traversing a graph data structure. The algorithm for performing a depth-first search is relatively simple—visit a vertex that has not already been visited, mark it as having been visited, then recursively visit the other unvisited vertices that are in the original vertex’s adjacency list. Pick a route, keep going, if you reach a dead end or an already visited vertex, backtrack to a previous vertex with unvisited adjacent neighbors.
---
+++ graph/depthFirstSearch/depthFormula.js
@@ -0,0 +1,64 @@
+/*
+    Adjency List Unordered Graph
+    *1. vertices (property) = Number of vertices in graph
+    *2. edges (property) = Number of edges in graph
+    *3. adj (property) = Array of vertices
+    Then add empty array for each vertex
+    *4. addEdge (function) = This function adds an edge between two vertices
+    *5. showGraph (function) = This function shows the graph
+    *6. marked (property) = stores visited vertices and initialize it to all false values.
+    *7. dfs (function) = This function performs depth first search
+*/
+
+class Graph {
+  constructor(v) {
+    this.vertices = v;
+    this.edges = 0;
+    this.adj = [];
+    for (let i = 0; i < this.vertices; i++) {
+      this.adj[i] = [];
+    }
+    this.marked = [];
+    for (let i = 0; i < this.vertices; ++i) {
+      this.marked[i] = false;
+    }
+  }
+
+  addEdge(v, w) {
+    this.adj[v].push(w);
+    this.adj[w].push(v);
+    this.edges++;
+  }
+
+  showGraph() {
+    for (let i = 0; i < this.vertices; i++) {
+      let output = i + " -> ";
+      for (let j = 0; j < this.adj[i].length; j++) {
+        output += this.adj[i][j] + " ";
+      }
+      console.log(output.trim());
+    }
+  }
+
+  dfs(v) {
+    this.marked[v] = true;
+    if (this.adj[v] !== undefined) {
+      console.log(`visited vertex : ${v}`);
+    }
+    for (let w of this.adj[v]) {
+      if (!this.marked[w]) {
+        this.dfs(w);
+      }
+    }
+  }
+}
+
+let g = new Graph(5);
+g.addEdge(0, 1);
+g.addEdge(0, 2);
+g.addEdge(1, 3);
+g.addEdge(2, 4);
+g.showGraph();
+g.dfs(4);
+
+console.log(g);
