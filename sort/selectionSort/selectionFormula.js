ff895f7cc4e3d62d3e20afecd30ec7dbd5de9d2b
mA4rK0 <onlymarko25@gmail.com>
2024-12-31 16:49:24 +0700

    make a selection sort algorithm.

--- sort/selectionSort/selectionFormula.js
+++ sort/selectionSort/selectionFormula.js
@@ -6,7 +6,7 @@
  *5. clear (function) = This function clears the array
  *6. setData (function) = This function sets the random numbers of the array
  *7. swap (function) = This function swaps two elements
- *8. bubbleSort (function) = This function sorts the array with bubble sort algorithm
+ *8. selectionSort (function) = This function sorts the array with selection sort algorithm
  */
 
 class CArray {
@@ -48,21 +48,22 @@ class CArray {
     arr[index2] = temp;
   }
 
-  bubbleSort() {
-    let numElements = this.dataStore.length;
-    for (let outer = numElements; outer >= 2; outer--) {
-      for (let inner = 0; inner <= outer - 1; inner++) {
-        if (this.dataStore[inner] > this.dataStore[inner + 1]) {
-          this.swap(this.dataStore, inner, inner + 1);
+  selectionSort() {
+    let min;
+    for (let outer = 0; outer <= this.dataStore.length - 2; outer++) {
+      min = outer;
+      for (let inner = outer + 1; inner <= this.dataStore.length - 1; inner++) {
+        if (this.dataStore[inner] < this.dataStore[min]) {
+          min = inner;
         }
       }
+      this.swap(this.dataStore, outer, min);
       console.log(this.toString());
     }
   }
 }
 
-let a = new CArray(10);
+let a = new CArray(5);
 a.setData();
-console.log(a);
 console.log(a.toString());
-a.bubbleSort();
+a.selectionSort();
